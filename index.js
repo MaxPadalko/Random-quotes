6444d43634e0c8faba64ede562ade16ed5107e90
Maxim Padalko <Salavat.city@inbox.ru>
2025-07-05 21:02:04 +0500

    MyFirstCommit

---
+++ index.js
@@ -0,0 +1,32 @@
+﻿const quotes = [
+  {
+    quote: "The only way to do great work is to love what you do",
+    author: "-Steve Jobs",
+  },
+  {
+    quote: "Innovation distinguishes between a leader and a follower",
+    author: "-Steve Jobs",
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life",
+    author: "-Steve Jobs",
+  },
+];
+
+const quoteElement = document.getElementById("quote");
+const quoteAuthorElement = document.getElementById("quote-author");
+const generateBtn = document.getElementById("generate-btn");
+
+function generateRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  const { quote, author: quoteAuthor } = randomQuote;
+  // const quote = randomQuote.quote;
+  // const quoteAuthor = randomQuote.author;
+  quoteElement.textContent = quote;
+  quoteAuthorElement.textContent = quoteAuthor;
+}
+
+generateBtn.addEventListener("click", generateRandomQuote);
+
+//generateRandomQuote();
