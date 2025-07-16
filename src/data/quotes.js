db67a44680dbf063469e01ace23ee14fb16f27c6
Maxim Padalko <Salavat.city@inbox.ru>
2025-07-17 00:14:29 +0500

    Creating ID for each quote

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,50 +1,60 @@
 ﻿const quotes = [
   {
+    id: "13843bf5",
     text: "The only way to do great work is to love what you do",
     author: "Steve Jobs",
     isFavorite: false,
   },
   {
+    id: "57f86338",
     text: "Innovation distinguishes between a leader and a follower",
     author: "Steve Jobs",
     isFavorite: false,
   },
   {
+    id: "c418f700",
     text: "Your time is limited, so don't waste it living someone else's life",
     author: "Steve Jobs",
     isFavorite: false,
   },
   {
+    id: "409c5fa6",
     text: "Be the change that you wish to see in the world.",
     author: "Mahatma Gandhi",
     isFavorite: false,
   },
   {
+    id: "541b53a0",
     text: "In the middle of difficulty lies opportunity.",
     author: "Albert Einstein",
     isFavorite: false,
   },
   {
+    id: "abedc37e",
     text: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
     author: "Ralph Waldo Emerson",
     isFavorite: false,
   },
   {
+    id: "be8ce882",
     text: "The only thing necessary for the triumph of evil is for good men to do nothing.",
     author: "Edmund Burke (attributed)",
     isFavorite: false,
   },
   {
+    id: "1f04a614",
     text: "Life is what happens when you're busy making other plans.",
     author: "John Lennon",
     isFavorite: false,
   },
   {
+    id: "29b9df10",
     text: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
     author: "Winston Churchill",
     isFavorite: false,
   },
   {
+    id: "f85f0b9c",
     text: "Whether you think you can, or you think you can't – you're right.",
     author: "Henry Ford",
     isFavorite: false,
--- src/handlers/favorites.js
+++ src/handlers/favorites.js
@@ -15,11 +15,11 @@ function toggleFavoriteBtnIcon(isFavorite, el) {
 
 function toggleFavorite(quote, btn, container) {
   quote.isFavorite = !quote.isFavorite;
-  const { text, author: quoteAuthor, isFavorite } = quote;
+  const { id, text, author: quoteAuthor, isFavorite } = quote;
   toggleFavoriteBtnIcon(isFavorite, btn);
 
   if (isFavorite) {
-    showFavoriteCard(text, quoteAuthor, container);
+    showFavoriteCard(id, text, quoteAuthor, container);
   } else {
     hideFavoriteCard(quote.text);
   }
@@ -30,9 +30,10 @@ function handleFavorite(isFavorite) {
   toggleFavoriteBtnIcon(isFavorite, favoriteBtn);
 }
 
-function showFavoriteCard(text, author, container) {
+function showFavoriteCard(id, text, author, container) {
   const favoriteCard = document.createElement("div");
   favoriteCard.classList.add("favorite-card");
+  favoriteCard.dataset.quoteId = id;
   favoriteCard.innerHTML = `
 <p>${text}</p>
 <p class="author">${author}</p>
@@ -40,13 +41,19 @@ function showFavoriteCard(text, author, container) {
   container.appendChild(favoriteCard);
 }
 
-function hideFavoriteCard(text) {
-  const favoriteCards = document.querySelectorAll(".favorite-card");
-  favoriteCards.forEach((card) => {
-    if (card.textContent.includes(text)) {
-      card.remove();
-    }
-  });
+function hideFavoriteCard(id) {
+  const card = document.querySelector(`.favorite-card[data-quote-id="${id}"]`);
+  console.log(card);
+  if (card) {
+    card.remove();
+  }
+
+  // const favoriteCards = document.querySelectorAll(".favorite-card");
+  // favoriteCards.forEach((card) => {
+  //   if (card.textContent.includes(text)) {
+  //     card.remove();
+  //   }
+  // });
 }
 
 export { handleFavorite, toggleFavorite, hideFavoriteBtn };
